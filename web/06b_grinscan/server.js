6915a35d49e9d6e2aa6599363b335f3388d88c31
Noobvie <admin@grin.money>
2026-05-09 07:55:53 -0400

    reverse neon color, fix backfill api method

--- web/06b_grinscan/server.js
+++ web/06b_grinscan/server.js
@@ -303,7 +303,7 @@ async function historicalBackfill() {
 
 async function pollBlocks() {
   try {
-    const tip       = await ownerApi('get_tip', []);
+    const tip       = await foreignApi('get_tip', []);
     const tipHeight = tip.height;
 
     // Stall detection
@@ -726,7 +726,7 @@ app.listen(config.port, '127.0.0.1', async () => {
 
   // Initial tip + backfill
   try {
-    const tip = await ownerApi('get_tip', []);
+    const tip = await foreignApi('get_tip', []);
     tipState.height = tip.height;
     tipState.hash   = tip.last_block_h;
     lastTipHeight   = tip.height;
